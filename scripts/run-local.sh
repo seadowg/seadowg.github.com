cd1fa056bc10242d0672605e5918af0eac8bbdf8
Callum Stott <callum@seadowg.com>
2026-06-01 08:45:24 +0100

    Don't publish port outside machine

--- scripts/run-local.sh
+++ scripts/run-local.sh
@@ -1,2 +1,2 @@
 source scripts/load-jekyll-version.sh
-docker run --volume="$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll:$JEKYLL_VERSION jekyll serve --watch --drafts
+docker run --volume="$PWD:/srv/jekyll" -p 127.0.0.1:4000:4000 -it jekyll/jekyll:$JEKYLL_VERSION jekyll serve --watch --drafts
